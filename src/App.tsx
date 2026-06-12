0705bba0e9af83fa540da2484e0ba1e4aee6e593
ZeitFuerLiebe <risitish@gmail.com>
2026-06-12 03:13:18 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { HomePage } from './pages/HomePage';
+import { getLocaleCode } from './assets/locales';
 
 export default function App() {
-  return <HomePage />;
+  const localeCode = getLocaleCode();
+  const isRtl = localeCode === 'he-IL';
+
+  return (
+    <div lang={localeCode} dir={isRtl ? 'rtl' : 'ltr'} data-locale={localeCode} data-dir={isRtl ? 'rtl' : 'ltr'}>
+      <HomePage />
+    </div>
+  );
 }
